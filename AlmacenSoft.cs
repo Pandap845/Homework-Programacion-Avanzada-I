8613815fcac797999cd9b1311faa00635164740d
VicPanda <aguilapanda.01@gmail.com>
2023-10-05 17:44:11 -0600

    Tercera prueba: login funcionando

--- AlmacenSoft.cs
+++ AlmacenSoft.cs
@@ -8,11 +8,15 @@ using static System.IO.Directory; // CRUD Directories
 using static System.IO.Path; //CREATE URL's C://Pipe//documents
 
 using static System.Environment;
-using Microsoft.VisualBasic; // Get the location, SpecialFolder, 
+
 using System.Security.Cryptography;
 using System.Text;
 using System.Xml.Serialization;
 
+//Alias
+
+
+using System.Formats.Asn1;
 
 namespace Software
 
@@ -20,8 +24,99 @@ namespace Software
 {
 
 
+    public class Login
+    {
 
-   
+       
+        static public bool ingreso( string? FilePath)
+        {
+
+            Almacenista almacenista= new();
+         
+           List<Almacenista> lista;
+           string? contraAlmacenista, NombreAlmacenista;
+
+        
+
+            if (Path.Exists(FilePath)) //verifica que exista y sea un archivo xml
+            {
+                WriteLine("Almacenista");
+                Write("Ingrese nombre de usuario:");
+                NombreAlmacenista = ReadLine();
+
+                Write("\nIngrese la contraseña:");
+                contraAlmacenista = ReadLine();
+
+                if(contraAlmacenista is not null)
+                {
+                contraAlmacenista = Encrypt.getSHA1(contraAlmacenista);
+                }
+                else{
+                    return false;
+                }
+
+                //deserializar el archivo xml que tiene a los almacenistas.
+
+               lista =  almacenista.xmlDeSerial<Almacenista>(FilePath);
+
+  //Verificar si corresponde con alguna contraseña
+                    foreach(var cuenta in lista)
+                    {
+                        if (cuenta.password ==  contraAlmacenista && cuenta.nombreCompleto == NombreAlmacenista)
+                        {
+                            return true;
+                        }
+
+                    }
+
+                    return false;
+
+              
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+    public class Encrypt
+    {
+
+        public static string getSHA1(string str) //Metodo que permite la encriptación en SHA1
+        {
+
+            SHA1 sha1 = SHA1.Create();
+
+            ASCIIEncoding encoding = new ASCIIEncoding(); //dar formato en ASCII
+
+            byte[]? stream = null; //almacena el hash del SHA1
+            StringBuilder sb = new StringBuilder(); //Mejor que un string normal 
+
+            stream = sha1.ComputeHash(encoding.GetBytes(str));
+            //Encriptación con el método SHA1. Como hace uso de un arreglo de bytes, es necesario el casteo
+
+            foreach (var variable in stream)
+            {
+                sb.AppendFormat($"{variable:x2}");
+            }
+
+            return sb.ToString();
+
+        }
+    }
+
 
 
 
@@ -31,103 +126,134 @@ namespace Software
         public int jsonSerial<T>(string? Name, List<T> lista); //Interfaz
 
         public int xmlSerial<T>(string? Name, List<T> lista);
-         
+
+
+        public List<T> xmlDeSerial<T>(string FilePath);
+
     }
 
 
-    public class Almacenista: ISerializar
+    public class Almacenista : ISerializar
     {
-       public string? nombreCompleto {get; set;} //Nombre completo del almacenista
+        public string? nombreCompleto { get; set; } //Nombre completo del almacenista
 
-        public  string ?password {get; set;}  //Contraseña del almacenista
-
-
-          
+        public string? password { get; set; }  //Contraseña del almacenista
 
 
-        public int jsonSerial<Almacenista>(string? Name, List<Almacenista> lista)  //Implementación de la interfaz, que permite serializar en json
+
+
+
+        public int jsonSerial<Almacenista>(string Name, List<Almacenista> lista)  //Implementación de la interfaz, que permite serializar en json
         {
-            if(Name is null)
+            if (!Path.Exists(Name) && Name is null)
             {
- 
+
                 return 0;
             }
-            else{
+            else
+            {
+
+
                 //specifie where is gonna be the file
-string jsonPath = Combine(GetFolderPath(SpecialFolder.Desktop), Name);
+              
 
-  using (StreamWriter jsonStream = File.CreateText(Name))
-    {
-        //Someone Who speak JSON
-    Newtonsoft.Json.JsonSerializer jss = new();
+                using (StreamWriter jsonStream = File.CreateText(Name))
+                {
+                    //Someone Who speak JSON
+                    Newtonsoft.Json.JsonSerializer jss = new();
 
-    //Serialize
-    jss.Serialize(jsonStream,  lista);
+                    //Serialize
+                    jss.Serialize(jsonStream, lista);
 
-}
-WriteLine(File.ReadAllText(Name));
-//EL objeto se crea dentro de los corchetes o paréntesis.
-
-        }
-         return 1;
+                }
+                WriteLine(File.ReadAllText(Name));
+                //EL objeto se crea dentro de los corchetes o paréntesis.
 
             }
+            return 1;
 
-          
-        
-  
+        }
+
 
         public int xmlSerial<Almacenista>(string? Name, List<Almacenista> lista)
         {
-            if(Name is null)
+            if (!Path.Exists(Name) && Name is null )
             {
                 return 0;
             }
-            else{
-
-            
-            
-//The XML Serializer NEEDS to know what type of data is going to be seralized.
-        XmlSerializer xs = new(type: lista.GetType()); //Se le indica el //Necesita saber que tipo de dato ocupa leer
+            else
+            {
 
 
-//Create a type
-string path = Combine(CurrentDirectory, Name); //For now, it just a file
 
-//Implicit declaration
-
-using (FileStream stream = File.Create(path))
-{
-
-    xs.Serialize(stream, lista);
-} //Llama automáticamente el close(), crear el archivo y abrir el archivo
-
-//1: take care of open on stream declaration
-//2: When } gets hit, Close file is called
-//3: Create file on declaration on Create(path)
+                //The XML Serializer NEEDS to know what type of data is going to be seralized.
+                XmlSerializer xs = new(type: lista.GetType()); //Se le indica el //Necesita saber que tipo de dato ocupa leer
 
 
-WriteLine($"\n{File.ReadAllText(path)}");
 
-          
+                //Implicit declaration
+
+                using (FileStream stream = File.Create(Name))
+                {
+
+                    xs.Serialize(stream, lista);
+                } //Llama automáticamente el close(), crear el archivo y abrir el archivo
+
+                //1: take care of open on stream declaration
+                //2: When } gets hit, Close file is called
+                //3: Create file on declaration on Create(path)
+
+
+                WriteLine($"\n{File.ReadAllText(Name)}");
+
+
             }
 
             return 0;
         }
+
+
+        public List<Almacenista> xmlDeSerial<Almacenista>(string FilePath)
+        {
+            List<Almacenista>? lista = new();
+            if (Path.Exists(FilePath))
+            {
+                using (FileStream xmlLoad = File.Open(FilePath, FileMode.Open))
+                {
+                    XmlSerializer xs = new(type: lista.GetType());
+                    //Desearilzar en una lista 
+
+                    lista = xs.Deserialize(xmlLoad) as List<Almacenista>;
+
+
+                }
+            }
+
+            return lista;
+
+        }
+
+
+
+        
+
     }
 
     public class Profesor
     {
 
-           private string ?nombreCompleto {get; set;}
+        public string? nombreCompleto { get; set; }
 
-           private  string ?Nomina {get; set;}
+        public string? Nomina { get; set; }
 
-           private string ?password {get; set;}
+        public string? password { get; set; }
 
-            public List<string>? materias = new();
+        public List<string> materias = new(){
 
-            public string? division;
+               "Computacion", "electronica", "Informatica", "Sistemas Digitales"
+        };
+
+        public string? division;
 
 
     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using static System.IO.Directory; // CRUD Directories
+
+using static System.IO.Path; //CREATE URL's C://Pipe//documents
+
+using static System.Environment;
+
 using Software;
+
+#region Inicializacion 
 
 Almacenista jefe = new();
 
@@ -8,21 +17,38 @@ List<Almacenista> almacenistasPro = new()
             new()
             {
                 nombreCompleto = "Pedro",
-                password = "123"
+                password = Encrypt.getSHA1("123")
                 
             },
 
             new()
             {
                 nombreCompleto = "Paco",
-                password = "321"
+                password = Encrypt.getSHA1("321")
                 
 
             }
 
 };
 
+   string dir = Combine(CurrentDirectory, "Almacenistas.json");
+   string dir2 = Combine(CurrentDirectory, "Almacenistas.xml");
 
-jefe.jsonSerial<Almacenista>("prueba.json", almacenistasPro);
+jefe.jsonSerial<Almacenista>(dir, almacenistasPro);
 
-jefe.xmlSerial<Almacenista>("prueba.xml", almacenistasPro);
+jefe.xmlSerial<Almacenista>(dir2, almacenistasPro);
+
+
+#endregion
+
+
+//Tiene que verificar que sea xml, que exista, que tenga algo, etc.
+if(Login.ingreso(dir2))
+{
+   
+}
+else{
+
+   
+}
+
